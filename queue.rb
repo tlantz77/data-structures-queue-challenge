843e396974c8d1a244ffd2be05e71c7e282a0d24
tlantz77 <tlantz@gmail.com>
2016-08-10 16:20:20 -0500

    add methods and tests for queue class

--- queue.rb
+++ queue.rb
@@ -2,4 +2,22 @@ require_relative '../data-structures-linked-list-challenge/linked_list.rb'
 
 class MyQueue < LinkedList
 
+  def peel
+    get(size - 1)
+  end
+
+  def enqueue(element)
+    insert_first(element)
+  end
+
+  def dequeue
+    popped_element = peel
+    remove_last
+    popped_element
+  end
+
+  def empty?
+    @size == 0
+  end
+
 end
--- queue_spec.rb
+++ queue_spec.rb
@@ -4,4 +4,44 @@ describe MyQueue do
 
   let(:queue) { MyQueue.new }
 
+  before(:each) do
+    queue.enqueue("a")
+    queue.enqueue("b")
+    queue.enqueue("c")
+  end
+
+  it "can instantiate a queue as a linked list" do
+    expect(queue.size).to eq(3)
+    expect(queue).to be_a(LinkedList)
+    expect(queue).to be_a(MyQueue)
+  end
+
+  it "can add a new element to the queue" do
+    expect { queue.enqueue("d") }.to change { queue.size }.from(3).to(4)
+  end
+
+  it "can return the first item in the queue" do
+    expect(queue.peel).to eq("a")
+  end
+
+  it "dequeue will remove the first element entered into the queue" do
+    queue.dequeue
+    expect(queue.peel).to eq("b")
+  end
+
+  it "dequeue will return the element removed from the queue" do
+    expect(queue.dequeue).to eq("a")
+  end
+
+  it "enqueuing a new element will not change the first element in the queue" do
+    queue.enqueue("d")
+    expect(queue.peel).to eq("a")
+  end
+
+  it "can check if queue is empty" do
+    empty_queue = MyQueue.new
+    expect(empty_queue.empty?).to be true
+    expect(queue.empty?).to be false
+  end
+
 end
